183a6faebdf41d4d6f5f87f18515ab55e51e8bac
RamoZanai <cs02697@uoi.gr>
2024-03-23 20:16:34 +0200

    eekana to print

--- shell.py
+++ shell.py
@@ -1,13 +1,14 @@
 
 
-import lexer
-
+import lexer 
 while True:
 
-    text = input("cpy>>>")
+
+    text = input("cpy=>>>")
     lexer_instance = lexer.Lexer(text)  # Create an instance of the Lexer class
     result ,error = lexer_instance.make_tokens()  # Call the make_toneks method
     if error:print(error.as_string())
     else:
         print(result)
+
 
